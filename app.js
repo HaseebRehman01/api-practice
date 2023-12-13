78ec17a96ba09080c921e22459f8718b88689c25
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-12-13 20:39:53 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+//api 
+
+// json
+// var catapiJson = '{"fact":"The average lifespan of an outdoor-only (feral and non-feral) is about 3 years; an indoor-only cat can live 16 years and longer. Some cats have been documented to have a longevity of 34 years.","length":192}';
+//json to js object
+// var catapiJsonIntoObj = JSON.parse(catapiJson)
+// console.log(catapiJsonIntoObj)
+
+//object to json we use this method when we made our own apis 
+// var obj  = {
+//     name : "haseeb" ,
+//     class : "matric"
+// }
+// let objtojson = JSON.stringify(obj)
+// console.log(objtojson)
